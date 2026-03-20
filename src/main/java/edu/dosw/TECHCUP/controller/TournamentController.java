9b19466a1146bc7c4b051895363a6559c661460c
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-03-19 22:26:18 -0500

    Feat: Implementacion Logs TorunamentController

--- src/main/java/edu/dosw/TECHCUP/controller/TournamentController.java
+++ src/main/java/edu/dosw/TECHCUP/controller/TournamentController.java
@@ -5,6 +5,7 @@ import edu.dosw.TECHCUP.controller.dto.TournamentResponseDTO;
 import edu.dosw.TECHCUP.controller.mapper.TournamentMapper;
 import edu.dosw.TECHCUP.core.model.Tournament;
 import edu.dosw.TECHCUP.core.service.TournamentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/torneos")
 public class TournamentController {
@@ -25,22 +27,27 @@ public class TournamentController {
 
     @PostMapping
     public ResponseEntity<TournamentResponseDTO> crearTorneo(@RequestBody TournamentRequestDTO request) {
+        log.info("POST /api/torneos - Request to create tournament");
         Tournament tournament = tournamentService.crearTorneo(request);
         TournamentResponseDTO response = tournamentMapper.toResponseDTO(tournament);
+        log.info("Tournament successfully created with id: {}", response.getId());
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
     public ResponseEntity<List<TournamentResponseDTO>> listarTorneos() {
+        log.debug("GET /api/torneos - Listing all tournaments");
         List<TournamentResponseDTO> torneos = tournamentService.listarTorneos()
                 .stream()
                 .map(tournamentMapper::toResponseDTO)
                 .collect(Collectors.toList());
+        log.debug("{} Tournaments were returned", torneos.size());
         return ResponseEntity.ok(torneos);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TournamentResponseDTO> obtenerTorneo(@PathVariable Long id) {
+        log.debug("GET /api/torneos/{} - Searching for tournament by Id", id);
         Tournament tournament = tournamentService.obtenerTorneoPorId(id);
         TournamentResponseDTO response = tournamentMapper.toResponseDTO(tournament);
         return ResponseEntity.ok(response);
@@ -50,8 +57,10 @@ public class TournamentController {
     public ResponseEntity<TournamentResponseDTO> cambiarEstado(
             @PathVariable Long id,
             @RequestParam String estado) {
+        log.debug("PATCH /api/torneos/{}/estado - Requesting a change of status to '{}'", id, estado);
         Tournament tournament = tournamentService.cambiarEstado(id, estado);
         TournamentResponseDTO response = tournamentMapper.toResponseDTO(tournament);
+        log.info("Tournament status {} successfully updated a '{}'", id, response.getEstado());
         return ResponseEntity.ok(response);
     }
 }
