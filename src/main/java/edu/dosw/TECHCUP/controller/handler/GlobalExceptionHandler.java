3f424f8bb3e5b768091634217b3e6cacba06497d
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-03-20 21:56:12 -0500

    Correccion error Handler

--- src/main/java/edu/dosw/TECHCUP/controller/handler/GlobalExceptionHandler.java
+++ src/main/java/edu/dosw/TECHCUP/controller/handler/GlobalExceptionHandler.java
@@ -8,11 +8,11 @@ import edu.dosw.TECHCUP.core.exception.UserValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TournamentNotFoundException.class)
