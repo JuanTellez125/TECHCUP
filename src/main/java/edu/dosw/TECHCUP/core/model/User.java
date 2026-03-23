616ebd937e5392e19521fdafcadb6f65b34733de
JuanTellez125 <juanestellez125@gmail.com>
2026-03-23 17:41:44 -0500

    feat: Refactor User

--- src/main/java/edu/dosw/TECHCUP/core/model/User.java
+++ src/main/java/edu/dosw/TECHCUP/core/model/User.java
@@ -1,14 +1,15 @@
 package edu.dosw.TECHCUP.core.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class User {
     private long id;
     private String name;
     private String email;
     private String password;
+    private Byte[] picture;
 
 }
