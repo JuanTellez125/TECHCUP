5f7db3b00f8c50ebd8eba88bce54675cb71bcb37
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-03-25 14:10:37 -0500

    FEAT: Implementacion MatchResultRequestDTO

---
+++ src/main/java/edu/dosw/TECHCUP/controller/dto/request/MatchResultRequestDTO.java
@@ -0,0 +1,22 @@
+package edu.dosw.TECHCUP.controller.dto.request;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MatchResultRequestDTO {
+
+    private int localScore;
+
+    private int awayScore;
+
+    private List<String> scorerIds;
+
+    private List<String> yellowCardPlayerIds;
+
+    private List<String> redCardPlayerIds;
+}
