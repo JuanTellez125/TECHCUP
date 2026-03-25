20ebd3717b3e8cc416f68f2ca646af72a4775b83
Cristian Guerrero <cristian.guerrero-b@mail.escuelaing.edu.co>
2026-03-25 12:28:53 -0500

    Feat: Implementacion Controller getMatchesByReferee

--- src/main/java/edu/dosw/TECHCUP/controller/MatchController.java
+++ src/main/java/edu/dosw/TECHCUP/controller/MatchController.java
@@ -1,4 +1,24 @@
 package edu.dosw.TECHCUP.controller;
 
+import edu.dosw.TECHCUP.controller.dto.response.MatchResponseDTO;
+import edu.dosw.TECHCUP.core.service.MatchService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/matches")
+@RequiredArgsConstructor
 public class MatchController {
+
+    private final MatchService matchService;
+
+    @Operation(summary = "Consultar partidos asignados a un arbitro")
+    @GetMapping("/referee/{refereeId}")
+    public ResponseEntity<List<MatchResponseDTO>> getMatchesByReferee (@PathVariable String refereeId){
+        return ResponseEntity.ok(matchService.getMatchesByReferee(refereeId));
+    }
 }
